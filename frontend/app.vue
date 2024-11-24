9500b8150d972d6852ca6059f281d042004b4cf0
kisamesang <kisamesang@gmail.com>
2024-11-24 10:49:47 +0700

    update รูปผู้โพสต์กระทู้

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,14 +1,21 @@
-<script>
+<script setup>
 // TODO: 4. ทำความรู้จัก useHead()
 // TODO: 5. ลอง console.log เพื่อดู log
-</script>
+console.log('Hello World');
 
+useHead({
+    title: 'Test Hello',
+});
+</script>
 
 <template>
     <!-- TODO: 1. ทำความรู้จัก template tag -->
-    <!-- TODO: 2. แก้ tailwind.config.js เพื่อให้ตอน build  -->
+    <!-- TODO: 2. ใช้ Tailwind CSS -->
+    <div class="flex flex-col items-center justify-center h-screen bg-brown-500">
+        <h1 class="text-4xl font-bold text-white">hello vue</h1>
+    </div>
 </template>
 
 <style lang="css">
-/** TODO: 3. ใส่ CSS ให้กับ HTML */
+/** TODO: 3. ลบ CSS แบบดั้งเดิม */
 </style>
